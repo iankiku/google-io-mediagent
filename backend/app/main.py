4c25af90c885a016ec57bb92a2c2571abec4796d
fischerxz <fisher262425@gmail.com>
2026-05-23 15:02:05 -0700

    add force_trigger endpoint plus read APIs and /debug/ravi page

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,7 @@ from app.domains.orchestration.router import router as orchestration_router
 from app.domains.ingestion.router import router as ingestion_router
 from app.domains.telegram.router import router as telegram_router, start_bot_polling
 from app.domains.interpreter.router import router as interpreter_router
+from app.domains.checkins.router import router as checkins_router, read_router, debug_router
 from app.core.db import initialize_database
 
 app = FastAPI(
@@ -28,6 +29,9 @@ app.include_router(orchestration_router)
 app.include_router(ingestion_router)
 app.include_router(telegram_router)
 app.include_router(interpreter_router)
+app.include_router(checkins_router)
+app.include_router(read_router)
+app.include_router(debug_router)
 
 @app.on_event("startup")
 def on_startup():
